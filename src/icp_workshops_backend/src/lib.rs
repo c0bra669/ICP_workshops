95797fd26de0733644c7e809e244ad94a6f66abf
Łukasz Tobiasiewicz <lukasztobiasiewicz@proton.me>
2025-03-20 11:28:47 +0000

    calculator

--- src/icp_workshops_backend/src/lib.rs
+++ src/icp_workshops_backend/src/lib.rs
@@ -2,3 +2,18 @@
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+#[ic_cdk::query]
+fn calculate(x: i32 ,y: i32,operator: String)->String{
+    let result = match operator.as_str() {
+        "+"=>Some(x+y),
+        "-"=>Some(x-y),
+        "*"=>Some(x*y),
+        "/"=>if y !=0{Some(x/y)}else{None},
+        _=>None
+    };
+    match  result {
+        Some(value)=> format!("Result:{}",value),
+        None => "invalid operator or deviding by zero".to_string(),
+    }
+
+}
